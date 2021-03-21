01697934bde1acf1449f5aebb3bf5b04216c0d59
AffanTheBest <affan207278@gmail.com>
2021-03-21 08:18:16 +0530

    Added support for multiple apps

--- script.ps1
+++ script.ps1
@@ -1,46 +1,93 @@
 $tasks = @(
-    "InstallWinGet" , "InstallChrome" , "InstallBrave" , "InstallVsCode" , "InstallTelegram" , "InstallVlc" , "Install7Zip" , "InstallMongoShell"
+    "InstallChrome" , "InstallBrave" , "InstallVsCode" , "InstallTelegram" , "InstallVlc" , "Install7Zip" , "InstallMongoShell"
 )
+# -----------------------------------------------------------------------------------------------------------------
 
-Write-Host "Installing WinGet...."
-Write-Host "===================================================="
-Function InstallWinGet {
-	Write-Host " "
-	# Import BitsTransfer module and download NetTestFile
-    Import-Module BitsTransfer 
-    Start-BitsTransfer https://raw.githubusercontent.com/CleanWin/Files/main/NetTestFile
-    # If the file exists, proceed with downloading WinGet files, else inform user about no internet connection.
-    If (Test-Path NetTestFile) {
-        Remove-Item NetTestFile
-        Write-Host "Downloading WinGet installation files..."
-        Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
-        Start-BitsTransfer https://github.com/CleanWin/Files/raw/main/Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-        Write-Host "Installing WinGet..."
-        Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -DependencyPath .\Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-        Remove-Item Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
-        Remove-Item Microsoft.VCLibs.140.00.UWPDesktop_14.0.29231.0_x64__8wekyb3d8bbwe.Appx
-        Write-Host "Winget Installed Successfully!"
-	} 
-	else {
-	  Write-Host "We can't connect to GitHub to download the installation files. Are you sure that your internet connection is working?"
-	}
+# Get the ID and security principal of the current user account
+Write-Host 'Getting ID and security principal...........';
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
+
+# Get the security principal for the administrator role
+Write-Host 'Getting security principal for administrative role...........';
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+
+# Check to see if we are currently running as an administrator
+Write-Host "Checking if we're currently as administrator or not...........";
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    # We are running as an administrator, so change the title and background colour to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(WinScript | Administrator)";
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue";
+    Clear-Host;
 }
+else {
+    # We are not running as an administrator, so relaunch as administrator
+    Write-Host "We are not running as an administrator, so relaunch as administrator...........";
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess);
+    Exit;
+}
+# -----------------------------------------------------------------------------------------------------------------
+
+Write-Host "Installing Chocolatey...."
+Write-Host "===================================================="
+Function InstallChocolatey {
+	Write-Host " "
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+Write-Host ' '
+Write-Host 'Done, Successfully installed chocolatey'
 
 Function Install7Zip {
     Write-Host ""
     Write-Host "Installing 7zip... "
-    winget install 7zip
+    choco install 7zip -y
     Write-Host "Done!"
     Write-Host ""
 }
 
 Function InstallChrome {
     Write-Host "Installing Chrome Stable..."
-    winget install Google.Chrome
+    choco install googlechrome -y
     Write-Host "Done!"
     Write-Host ""
 }
 
-Invoke-Expression InstallWinGet
+Function InstallFireFox {
+    Write-Host "Installing Firefox Stable..."
+    choco install firefox -y
+    Write-Host "Done!"
+    Write-Host ""
+}
+
+Function InstallVscode {
+    Write-Host "Installing VSCode..."
+    choco install vscode -y
+    Write-Host "Done!"
+    Write-Host ""
+}
+
+Function InstallTelegram {
+    Write-Host "Installing Telegram Desktop..."
+    choco install telegram -y
+    Write-Host "Done!"
+    Write-Host ""
+}
+
+Function InstallVlc {
+    Write-Host "Installing VLC..."
+    choco install vlc -y
+    Write-Host "Done!"
+    Write-Host ""
+}
+
+Invoke-Expression InstallChocolatey
+Invoke-Expression InstallTelegram
+Invoke-Expression InstallVlc
 Invoke-Expression Install7Zip
 Invoke-Expression InstallChrome
+Invoke-Expression InstallaFireFox
+Invoke-Expression InstallVscode
